61bd22ab91c3aa8cb013c642f5fb694c95038f6f
Jonaskar-64Bit <jonas.krueger.eu@gmx.de>
2026-01-01 16:00:14 +0100

    Added new  Item Section and import HTML Elements in JS Code

---
+++ script.js
@@ -0,0 +1,5 @@
+const inventoryList = document.getElementById('inventory-list');
+const addItemButton = document.getElementById('add-item-btn');
+const itemNameInput = document.getElementById('item-name-input');
+const itemQuantityInput = document.getElementById('item-quantity-input');
+const errorField = document.getElementById('errorField');
